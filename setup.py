c6ff8b4bd6213cf50b1dea4a014957501e916f13
Jeffrey Edwin Seifried <jeffrey.seifried@gmail.com>
2017-04-16 11:45:43 -0700

    Moved colors to colors package Cleaned up LOCATION formatting

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ from switch import __version__
 setup(
     author=__author__,
     author_email=__email__,
+    dependency_links=[
+        'https://github.com/jeffseif/colors.git#egg=colors',
+    ],
     install_requires=[
         'beautifulsoup4>=4.5.3',
         'requests>=2.9.1',
--- switch/web_session.py
+++ switch/web_session.py
@@ -1,10 +1,14 @@
+from colors import BLUE
+from colors import GREEN
+from colors import RED
+from colors import YELLOW
+from colors import WHITE
+
 from switch.logger import Logger
 
 
-FAILURE = '\033[1;31m✗\033[0m'
-LOCATION = '\033[1;33m{:s}\033[0m: \033[1;90m{:s}\033[0m'
-SUCCESS = '\033[1;32m✓\033[0m'
-STATUS = '{:s} \033[1;37m{:s}\033[0m'
+FAILURE = RED('✗')
+SUCCESS = GREEN('✓')
 
 
 class WebSession(Logger):
@@ -17,6 +21,6 @@ class WebSession(Logger):
         results = list(self.check_response_for_product(response, self.product_id))
         status = SUCCESS if results else FAILURE
 
-        print(STATUS.format(status, self.product_description))
-        for result in results:
-            print(LOCATION.format(*result))
+        print(' '.join((status, WHITE(self.product_description))))
+        for left, right in results:
+            print(YELLOW(left) + ': ' + BLUE(right))
